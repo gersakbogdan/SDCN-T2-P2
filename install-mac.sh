588b3a428ab96fd248c960ca84460522e0da91ba
Bogdan Gersak <bogdan@gersak.ro>
2017-07-09 14:00:49 +0200

    Fix openssl not found error

--- install-mac.sh
+++ install-mac.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 brew install openssl libuv cmake zlib
-git clone https://github.com/uWebSockets/uWebSockets 
+git clone https://github.com/uWebSockets/uWebSockets
 cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
 cd build
-cmake ..
-make 
+cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DOPENSSL_LIBRARIES=/usr/local/opt/openssl/lib ..
+make
 sudo make install
 cd ..
 cd ..
